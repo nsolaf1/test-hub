d6abf5ca93ab2a4c3f3181a839a5dc12ce75aed3
NSOLAf1 <nsolaf1@gmail.com>
2024-10-17 16:31:08 +0600

    t page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Hello WOrld </h1></>
+    <h1>Hello WOrld </h1>
+    <h1>Hello WOrld </h1>
+    </>
   );
 }
